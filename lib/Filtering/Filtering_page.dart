fad6451cccd8fb885244e729d420eb86e7f97267
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-02-21 13:51:45 +0200

    adding Transaction to app

--- lib/Filtering/Filtering_page.dart
+++ lib/Filtering/Filtering_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +13,7 @@ class _FilteringPageState extends State<FilteringPage> {
   Future<void> initalData() async {
     CollectionReference users = FirebaseFirestore.instance.collection('users');
     QuerySnapshot querySnapshot =
-        await users.orderBy('age',descending: false).startAfter([22]).get();
+        await users.orderBy('age', descending: false).startAfter([14]).get();
     querySnapshot.docs.forEach((element) {
       data.add(element);
       setState(() {});
@@ -39,6 +38,30 @@ class _FilteringPageState extends State<FilteringPage> {
           itemCount: data.length,
           itemBuilder: (context, index) {
             return UserInfo(
+              onTap: () {
+                DocumentReference documentReference = FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(data[index].id);
+
+                FirebaseFirestore.instance.runTransaction((transaction) async {
+                  DocumentSnapshot snapshot =
+                      await transaction.get(documentReference);
+
+                  if (snapshot.exists) {
+                    var snapShotData = snapshot.data();
+
+                    if (snapShotData is Map<String, dynamic>) {
+                      int money = snapShotData['money'] + 100;
+                      transaction.update(documentReference, {'money':money});
+                    }
+                  }
+                }).then((value){
+                          Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(
+                        builder: (context) => const FilteringPage()),
+                    (route) => false);});
+              },
+              money: data[index]['money'],
               age: data[index]['age'],
               name: data[index]['name'],
             );
@@ -48,29 +71,60 @@ class _FilteringPageState extends State<FilteringPage> {
 }
 
 class UserInfo extends StatelessWidget {
-  const UserInfo({super.key, required this.age, required this.name});
+  const UserInfo(
+      {super.key,
+      required this.age,
+      required this.name,
+      required this.money,
+      this.onTap});
   final int age;
   final String name;
+  final double money;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            name,
-            style: const TextStyle(fontSize: 60),
-          ),
-          Opacity(
-              opacity: .6,
-              child: Text(
-                'age :$age',
-                style: const TextStyle(fontSize: 22),
-              )),
-          const Divider(),
-        ],
+      child: InkWell(
+        //dh el 7al ely momken y3ml m4akel lano y3tamed 3la il data ely 3la el 4a4a
+        // onTap: () {
+        //         DocumentReference doc = FirebaseFirestore.instance
+        //             .collection('users')
+        //             .doc(data[index].id);
+
+        //         doc.update({'money':data[index]['money']+100});
+
+        //         Navigator.of(context).pushAndRemoveUntil(
+        //             MaterialPageRoute(
+        //                 builder: (context) => const FilteringPage()),
+        //             (route) => false);
+        //       },
+        //el 7al el salem fo2
+        onTap: onTap,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              name,
+              style: const TextStyle(fontSize: 60),
+            ),
+            Align(
+                heightFactor: 0,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '$money\$',
+                  style: TextStyle(fontSize: 22, color: Colors.red),
+                )),
+            Opacity(
+                opacity: .6,
+                child: Text(
+                  'age :$age',
+                  style: const TextStyle(fontSize: 22),
+                )),
+            const Divider(),
+          ],
+        ),
       ),
     );
   }
