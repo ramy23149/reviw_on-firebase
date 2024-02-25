20e3034ac06554128f0f74455f58b5d09eb80496
ramy <Phrankie@DESKTOP-TL7U6VL.(none)>
2024-02-25 18:30:18 +0200

    useing on message methoud to control the notification when the app in forgraound mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:market_app/add_category/add_category.dart';
- import 'package:market_app/auth/login.dart';
+import 'package:market_app/auth/login.dart';
 import 'package:market_app/auth/signup.dart';
-import 'package:market_app/cloud_storage/cloud_storage.dart';
 import 'package:market_app/home_page.dart';
 import 'package:market_app/notifecations/notifecation.dart';
 import 'firebase_options.dart';
+
 void main() async {
-await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
   runApp(MyApp());
 }
 
@@ -24,6 +25,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
 //   @override
 //   void initState() {
 // FirebaseAuth.instance
@@ -41,11 +43,13 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(backgroundColor: Colors.grey[50],
-        titleTextStyle: TextStyle(fontSize: 32,fontWeight: FontWeight.bold,color: Colors.orange),
-       iconTheme: IconThemeData(color: Colors.orange,size: 32)
-        )
-      ),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.grey[50],
+              titleTextStyle: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange),
+              iconTheme: IconThemeData(color: Colors.orange, size: 32))),
       debugShowCheckedModeBanner: false,
       // home:  (FirebaseAuth.instance.currentUser!=null && FirebaseAuth.instance.currentUser!.emailVerified) ? Homepage():
       //     Login(),
@@ -54,7 +58,7 @@ class _MyAppState extends State<MyApp> {
         "signup": (context) => SignUp(),
         "login": (context) => Login(),
         "HomePage": (context) => Homepage(),
-        'AddCategoryPage':(context) => AddCategoryPage()
+        'AddCategoryPage': (context) => AddCategoryPage()
       },
     );
   }
--- lib/notifecations/notifecation.dart
+++ lib/notifecations/notifecation.dart
@@ -1,23 +1,63 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Notfi extends StatefulWidget {
-  const Notfi({super.key});
+  const Notfi({
+    super.key,
+  });
 
   @override
   State<Notfi> createState() => _NotfiState();
 }
 
 class _NotfiState extends State<Notfi> {
+  String? token;
   Future<void> getToken() async {
-    String? myToken = await FirebaseMessaging.instance.getToken();
-    print(myToken);
+    token = await FirebaseMessaging.instance.getToken();
+    //print(token);
+  }
+
+  requestpermation() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      // Handle foreground messages here
+      if (message.notification != null) {
+        AwesomeDialog(
+                context: context,
+                title: message.notification!.title,
+                body: Text('${message.notification!.body}'),
+                dialogType: DialogType.info)
+            .show();
+      }
+    });
+    requestpermation();
     getToken();
   }
 
@@ -28,7 +68,45 @@ class _NotfiState extends State<Notfi> {
         title: const Text('Notefication'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(),
+      body: MaterialButton(
+        onPressed: () {
+          sendMessage(token!, 'hallo', 'how are you');
+        },
+        child: const Text('sent massage'),
+      ),
     );
+  }
+}
+
+Future<void> sendMessage(String deviceToken, String title, String body) async {
+  // Construct the request body
+  var bodyData = jsonEncode({
+    'notification': {
+      'title': title,
+      'body': body,
+    },
+    'to': deviceToken,
+  });
+
+  // Construct the request headers
+  var headers = {
+    'Content-Type': 'application/json',
+    'Authorization':
+        'key=AAAAI0x5Gtc:APA91bFwS1vqIZEV7Ql7i9gLFOAKHDHCKzEcQF5-sWFudJ0-nBMRsurLbOtMYy4RhMYDDBO-qkTxfKN7Fh9JXILPfP8Fz-2twtlpdmMg2cF5oRRHtguWsf4h0GsxOYKWjJ4EUPuU_IEP',
+  };
+
+  // Send the request
+  var response = await http.post(
+    Uri.parse('https://fcm.googleapis.com/fcm/send'),
+    headers: headers,
+    body: bodyData,
+  );
+
+  // Check the response status
+  if (response.statusCode == 200) {
+    print('Message sent successfully.');
+  } else {
+    print('Failed to send message. Status code: ${response.statusCode}');
+    print('Response body: ${response.body}');
   }
 }
